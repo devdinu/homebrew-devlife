be6c1defca053dbbc5652487c903c97f42f15c17
Dinesh Kumar <dinesh.kumar@go-jek.com>
2019-04-06 09:18:50 +0530

    Using tag instead of SHA

--- Formula/gcloud-client.rb
+++ Formula/gcloud-client.rb
@@ -1,18 +1,15 @@
 class GcloudClient < Formula
   desc "gcloud-client tool which does more for devs than gcloud"
-  url "github.com/devdinu/gcloud-client"
-  sha256 "20f0085f6fb83d3c49f2a6ef325a7e27bb277c55e26692a87b6e02b79f37bb2a"
-
+  url "https://github.com/devdinu/gcloud-client/releases/tag/v0.1.0"
 
   depends_on "tmux"
   depends_on "go" => :build
 
   def install
-    system "scripts/install.sh"
-    bin.install "gcloud-client"
+    system "sh", "scripts/install.sh"
   end
 
   test do
-    system "command" "-v" "gcloud-client"
+    system "command", "-v", "gcloud-client"
   end
 end
