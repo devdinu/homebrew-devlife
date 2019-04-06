bc8b0a72b7a5988cf4cb0878071d4e241a6bd1cc
Dinesh Kumar <dinesh.kumar@go-jek.com>
2019-04-06 12:44:47 +0530

    bin.install to create binary

--- Formula/gcloud-client.rb
+++ Formula/gcloud-client.rb
@@ -1,12 +1,17 @@
+require 'tmpdir'
+
 class GcloudClient < Formula
   desc "gcloud-client tool which does more for devs than gcloud"
-  url "https://github.com/devdinu/gcloud-client/releases/tag/v0.1.0"
+  url "https://github.com/devdinu/gcloud-client.git", :using => :git, :tag => "v0.1.0"
+  version "0.1.0"
 
   depends_on "tmux"
   depends_on "go" => :build
 
   def install
-    system "sh", "scripts/install.sh"
+    system "sh", "scripts/install.sh", Dir.tmpdir
+    # tmuxinator couldn't be installed with dir permissions issue
+    bin.install "#{Dir.tmpdir}/gcloud-client"
   end
 
   test do
